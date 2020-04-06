bb0815deacb4421b2e9fa6078274fdf3a2a9dc2d
Anthony De Guzman <anthony@Anthonys-MacBook-Pro.local>
2020-04-05 22:35:26 -0700

    updating add.py

--- add.py
+++ add.py
@@ -1,7 +1,11 @@
-A = 1
-B = 2
-C = A + B
+"""
+This python scrupt expects 2 parameters and outputs it's addition
+"""
 
-print(C)
+#function add
+def add(param_x, param_y):
+    """Return the the addition of the parameters"""
+    return param_x+param_y
 
-
+if __name__ == "__main__":
+    print(add(1, 2))
